d3b8b0e925877cb9a374574266e9a8360d66d665
genagain <ohta.g@husky.neu.edu>
2016-04-02 17:41:44 -0400

    fixed syntax error

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 callers = {
     "+12038485151": "Paulina",
-    "+13479860720": "Gen"
+    "+13479860720": "Gen",
 }
 
 @app.route("/", methods=['GET', 'POST'])
@@ -13,7 +13,7 @@ def hello_monkey():
   """Respond and greet the caller by name."""
   from_number = request.values.get('From', None)
   if from_number in callers:
-    message = callers[from_number] = ", thanks for the message!"
+    message = callers[from_number] + ", thanks for the message!"
   else:
     message = "Monkey, thanks for the message!"
 
